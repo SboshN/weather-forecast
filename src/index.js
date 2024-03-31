929f3830cb01aef1d248e96b5ca7d25159d7b62a
User <ssncaoayi@gmail.com>
2024-03-31 20:47:11 +0200

    removed repeated html and added into js

--- src/index.js
+++ src/index.js
@@ -56,3 +56,18 @@ function refreshWeather(response) {
   searchFormElement.addEventListener("submit", handleSearchSubmit);
   
   searchCity("Paris");
+
+  let forecast = document.querySelector("#forecast");
+
+  forecast.innerHTML = `
+  <div class="weather-forecast-day">
+  <div class="weather-forecast-date">Tue</div>
+  <div class="weather-forecast-icon">🌤️</div>
+  <div class="weather-forecast-temperatures">
+    <div class="weather-forecast-temperature">
+      <strong>15º</strong>
+    </div>
+    <div class="weather-forecast-temperature">9º</div>
+  </div>
+</div>  
+  `;
