92be540889e408115b833d98b532f5ecd4f611bb
User <ssncaoayi@gmail.com>
2024-03-31 21:05:08 +0200

    adding loop and array to js

--- src/index.js
+++ src/index.js
@@ -51,23 +51,33 @@ function refreshWeather(response) {
   
     searchCity(searchInput.value);
   }
+
+  function displayForecast() {
+    let forecast = document.querySelector("#forecast");
+  let days = ["Tue", "Wed", "Thu", "Fri", "Sat"]
+
+  days.forEach(function(day){
+    forecast.innerHTML = `
+        <div class="weather-forecast-day">
+        <div class="weather-forecast-date">${day}</div>
+        <div class="weather-forecast-icon">🌤️</div>
+        <div class="weather-forecast-temperatures">
+            <div class="weather-forecast-temperature">
+            <strong>15º</strong>
+            </div>
+            <div class="weather-forecast-temperature">9º</div>
+        </div>
+        </div>  
+        `;
+  });
+
+  
+  };
   
   let searchFormElement = document.querySelector("#search-form");
   searchFormElement.addEventListener("submit", handleSearchSubmit);
   
   searchCity("Paris");
+  displayForecast();
 
-  let forecast = document.querySelector("#forecast");
-
-  forecast.innerHTML = `
-  <div class="weather-forecast-day">
-  <div class="weather-forecast-date">Tue</div>
-  <div class="weather-forecast-icon">🌤️</div>
-  <div class="weather-forecast-temperatures">
-    <div class="weather-forecast-temperature">
-      <strong>15º</strong>
-    </div>
-    <div class="weather-forecast-temperature">9º</div>
-  </div>
-</div>  
-  `;
+  
