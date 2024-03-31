c1ccb2947ec9f0c4dc99b701d09c19e65814611c
User <ssncaoayi@gmail.com>
2024-03-31 21:52:45 +0200

    added forecastapi

--- src/index.js
+++ src/index.js
@@ -16,6 +16,8 @@ function refreshWeather(response) {
     windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
     temperatureElement.innerHTML = Math.round(temperature);
     iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" class="weather-app-icon" />`;
+
+    getForecast(response.data.city);
   }
   
   function formatDate(date) {
@@ -41,7 +43,7 @@ function refreshWeather(response) {
   
   function searchCity(city) {
     let apiKey = "b2a5adcct04b33178913oc335f405433";
-    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
     axios.get(apiUrl).then(refreshWeather);
   }
   
@@ -52,13 +54,21 @@ function refreshWeather(response) {
     searchCity(searchInput.value);
   }
 
-  function displayForecast() {
-    let forecast = document.querySelector("#forecast");
-  let days = ["Tue", "Wed", "Thu", "Fri", "Sat"];
-  let forecastHtml = ";"
+function getForecast(city) {
+    let apiKey = "b2a5adcct04b33178913oc335f405433";
+    let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric`;
+    axios(apiUrl).then(displayForecast);
+   };
+
+  function displayForecast(response) {
+        console.log(response.data);
+        
+        let forecast = document.querySelector("#forecast");
+        let days = ["Tue", "Wed", "Thu", "Fri", "Sat"];
+        let forecastHtml = "";
 
   days.forEach(function(day){
-    forecastHtml = forecastHtml +
+    forecastHtml +=
         `
         <div class="weather-forecast-day">
         <div class="weather-forecast-date">${day}</div>
@@ -82,6 +92,7 @@ function refreshWeather(response) {
   searchFormElement.addEventListener("submit", handleSearchSubmit);
   
   searchCity("Paris");
-  displayForecast();
+  getForecast("Paris");
+  
 
   
